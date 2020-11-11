38c79f8335a549ecaaf24d5b3eaa308b97f88968
Henry Doan <henrydoan96@gmail.com>
2020-11-10 20:06:45 -0700

    middle

---
+++ ruby_day1.rb
@@ -0,0 +1,181 @@
+# comment 
+# notes 
+
+# command ? - comment and uncomment inside of vscode
+# <p>
+# hello
+# </p>
+
+# Data types
+# String 
+#   " " 
+#   ' '
+#   'string'
+#   '1'
+#   'true'
+#   "don't"
+#   # '\'
+
+# Integer
+#   1
+#   -1234
+#   0
+
+# Float
+#   3.14
+#   0.1234
+
+# Boolean
+#   true false
+#    1    0
+# Array
+#   ['bob', 'jill', 'james']
+#   [ 1, 2, 3]
+#   [ 'bob', 2, true ]
+# Hashes
+#   { key: value }
+#   { first_name: 'henry' }
+#   { amount: 100 }
+  
+# nil 
+#   no value 
+  
+# undefined 
+#   not created yet
+
+# Date time
+#   11:10:12:UTC:123
+#   11/11/21
+
+# Objects
+#   noun - person place or thing
+#   hash - key value pair
+
+# Inputs and outputs
+# output info into the terminal
+# puts 'hi' #shows up in another line
+# print 'hi' #show in the same line
+# p 'hi' #same line and datatype
+
+# puts { first: 'bob'}
+
+# inputs user info 
+# gets
+# puts gets
+
+# puts true
+# puts 1 + 3
+# + - * / pemdas
+  # % - modulus
+  # 4 % 2 return the remainder
+# puts 1 + (3 + 4)
+
+# puts 2 % 2
+# puts 4 % 2
+# puts 6 % 2
+# puts 8 % 2
+
+# puts 1 + 1.23
+# puts 1 + '1'
+
+# puts 'hi' + 'world'
+# puts 'hi world'
+# String concatenation
+  # combining string to make a bigger string
+
+
+# = Assignment
+
+# == equal value 
+# puts 1 == 2
+# puts 1 == 1
+# puts 1 == '1'
+
+# === equal value and datatype
+
+# != not equal 
+# !== 
+
+# Variable 
+# store data 
+# to make a variable you have to have a name, assignment and what it 
+# is you are assigning it to.
+  # with naming a variable
+  #   scope  - where you have access to with the variable
+  #   dynamic 
+  #     x 
+  #     first_name
+  #   not a reserved word 
+  #     class
+  #     puts 
+  #     method
+  #     ruby 
+  #   Concise 
+  #   connect with a _ for more than one word
+  #   first_name
+  #   last_name_and_age
+# name    assignment  object 
+# greeting = 'Hello'
+# puts greeting
+# String interpolation
+# puts "#{greeting} world!"
+
+# puts 'enter your name'
+# user_name = gets
+# puts "hello #{user_name}"
+
+# scope - where you have access to the variable
+#   where youre able to use the variable
+
+# Foo = 'constant'
+#   constant
+#   Starts with 1st capital
+#   scoped to a file
+#   you shouldn't change this variable 
+
+# foo = 'local'
+#   local
+#   start with a lowercase 
+#   _ 
+#   scoped in the code block 
+
+#   def methods
+#     foo 
+#   end
+
+# @foo = 'instance variable'
+#   instance
+#   start with @
+#   file
+#   running instance or class
+ 
+#   class Person
+#     @foo
+#   end
+
+# @@foo = 'class'
+#   class
+#   @@
+#   scoped file
+#   class
+
+# Warning don't ever use 
+# $foo = 'global'
+#     global
+#     $
+#     machine wide 
+
+# Foo = 'h1'
+
+# def print_foo
+#   foo = 'h2'
+#   @foo = 'h3'
+#   puts foo
+# end
+
+# print_foo
+# puts Foo
+# puts @foo
+
+# String first_name = 'bob'
+# first_name = 'bob'
